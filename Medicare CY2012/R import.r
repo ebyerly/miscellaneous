8b6339d806dea579d47a92c079bf0a3407e96da1
ElizabethAB <elizabeth.byerly@gmail.com>
2014-04-10 14:26:14 -0400

    Fix

--- Medicare CY2012/R import.r
+++ Medicare CY2012/R import.r
@@ -6,8 +6,11 @@ drv <- dbDriver('PostgreSQL')
 # define connection
 medicare_conn <- dbConnect(drv, user='postgres',password='****',dbname='medicare2012',host='localhost',port='5432')
 
-# example query
+# Example query (returns a result set object, or gibberish for our purposes)
 query <- dbSendQuery(medicare_conn, 'SELECT * FROM medicare_original limit 10')
 
-# Extracting from query
-example_dataframe <- fetch(rs)
+# Extracting from query (converts the RSO into a dataframe)
+example_dataframe <- fetch(query)
+
+# Performs dbSendQuery, fetch, and dbClearResult functions in one.
+dbGetQuery(medicare_conn, 'Select * from medicare_original limit 5')
